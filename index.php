17274feb86ffa05e0bd7bfdb359c675c8740a458
federicascognamiglio <scognamigliofede99@gmail.com>
2025-03-17 19:19:18 +0100

    feat: navbar added

--- index.php
+++ index.php
@@ -16,12 +16,35 @@ $movie3 = new Movie('Interstellar', 'Christopher Nolan', 2014, 'https://www.them
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies</title>
+
+    <!-- Bootstrap -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+
 </head>
+
 <body>
-    
+    <!-- Navbar -->
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+    </nav>
+    <!-- /Navbar -->
+
+    <!-- Main -->
+    <main>
+        <div class="conatiner">
+            <h1 class="text-center mt-5">MY MOVIES</h1>
+        </div>
+    </main>
+    <!-- /Main -->
+
 </body>
+
 </html>
